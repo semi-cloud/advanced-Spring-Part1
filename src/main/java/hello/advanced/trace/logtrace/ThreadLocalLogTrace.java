ea5226194fd4a2e18cc3b2d58b90f0be560958c4
semi-cloud <sksmsrkdtpal@naver.com>
2023-01-04 15:10:10 +0900

    :sparkles: 로그 추적기 V4: 쓰레드 로컬 동기화
    ThreadLocal을 통해 동시성 문제 해결

---
+++ src/main/java/hello/advanced/trace/logtrace/ThreadLocalLogTrace.java
@@ -0,0 +1,72 @@
+package hello.advanced.trace.logtrace;
+
+import hello.advanced.trace.TraceId;
+import hello.advanced.trace.TraceStatus;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ThreadLocalLogTrace implements LogTrace {
+
+    private static final String START_PREFIX = "-->";
+    private static final String COMPLETE_PREFIX = "<--";
+    private static final String EX_PREFIX = "<X-";
+
+    private ThreadLocal<TraceId> traceHolder = new ThreadLocal<>();   // 쓰레드 로컬
+
+    @Override
+    public TraceStatus begin(String message) {
+        syncTraceId();
+        TraceId traceId = traceHolder.get();
+        long startTimeMs = System.currentTimeMillis();
+        log.info("[{}] {}{}", traceId.getId(), addSpace(START_PREFIX, traceId.getLevel()), message);
+        return new TraceStatus(traceId, startTimeMs, message);
+    }
+
+    private void syncTraceId() {
+        TraceId traceId = traceHolder.get();
+        if (traceHolder == null) {   // 최초 호출이라면 새로 생성
+            traceHolder.set(new TraceId());
+        } else {                     // 직전 로그가 있다면 동기화 후에 레벨만 증가
+            traceHolder.set(traceId.createNextId());
+        }
+    }
+
+    @Override
+    public void end(TraceStatus status) {
+        complete(status, null);
+    }
+
+    @Override
+    public void exception(TraceStatus status, Exception e) {
+        complete(status, e);
+    }
+
+    private void complete(TraceStatus status, Exception e) {
+        Long stopTimeMs = System.currentTimeMillis();
+        long resultTimeMs = stopTimeMs - status.getStartTimeMs();
+        TraceId traceId = status.getTraceId();
+        if (e == null) {
+            log.info("[{}] {}{} time={}ms", traceId.getId(), addSpace(COMPLETE_PREFIX, traceId.getLevel()), status.getMessage(), resultTimeMs);
+        } else {
+            log.info("[{}] {}{} time={}ms ex={}", traceId.getId(), addSpace(EX_PREFIX, traceId.getLevel()), status.getMessage(), resultTimeMs, e.toString());
+        }
+        releaseTraceId();
+    }
+
+    private void releaseTraceId() {
+        TraceId traceId = traceHolder.get();
+        if (traceId.isFirstLevel()) {   // 최초 호출이면 아예 제거
+            traceHolder.remove();       // 해당 스레드의 전용 보관소 초기화
+        } else {                            // 최초 호출이 아니라면 동기화 후에 레벨만 감소
+            traceHolder.set(traceId.createPreviousId());
+        }
+    }
+
+    private Object addSpace(String prefix, int level) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < level; i++) {
+            sb.append( (i == level - 1) ? "|" + prefix : "|   ");
+        }
+        return sb.toString();
+    }
+}
--- src/test/java/hello/advanced/trace/threadlocal/ThreadLocalServiceTest.java
+++ src/test/java/hello/advanced/trace/threadlocal/ThreadLocalServiceTest.java
@@ -1,23 +1,23 @@
 package hello.advanced.trace.threadlocal;
 
-import hello.advanced.trace.threadlocal.code.FieldService;
+import hello.advanced.trace.threadlocal.code.ThreadLocalService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
-public class FieldServiceTest {
+public class ThreadLocalServiceTest {
 
-    private FieldService fieldService = new FieldService();
+    private ThreadLocalService service = new ThreadLocalService();
 
     @Test
     void field() {
         log.info("main start");
         Runnable userA = () -> {
-            fieldService.logic("userA");
+            service.logic("userA");
         };
 
         Runnable userB = () -> {
-            fieldService.logic("userB");
+            service.logic("userB");
         };
 
         Thread threadA = new Thread(userA);
@@ -27,13 +27,14 @@ public class FieldServiceTest {
 
         threadA.start();
         // 1. 동시성 문제가 발생하지 않는 경우
-        sleep(2000);       // A가 완전히 끝나고 나서 B 시작
+        sleep(2000);
         // 2. 동시성 문제가 발생하는 경우 : 저장한 데이터와 조회한 데이터가 다른 문제
-        sleep(100);        // A가 끝나기 전에 B가 시작되므로, A가 조회하기 전에 B의 값이 저장되어 A 조회 시 B가 출력됌
+        sleep(100);
         threadB.start();
 
         sleep(3000);     // 메인 쓰레드 종료 대기
         log.info("main exit");
+
     }
 
     private void sleep(long millis) {
