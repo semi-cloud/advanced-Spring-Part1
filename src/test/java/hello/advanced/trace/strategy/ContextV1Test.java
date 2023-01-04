a29b49cd1b8f344597ca71c3341c08cbac7ea9cd
semi-cloud <sksmsrkdtpal@naver.com>
2023-01-04 17:15:49 +0900

    :white_check_mark: 전략 패턴 테스트 코드 추가

---
+++ src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -0,0 +1,54 @@
+package hello.advanced.trace.strategy;
+
+import hello.advanced.trace.strategy.code.strategy.ContextV1;
+import hello.advanced.trace.strategy.code.strategy.Strategy;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ContextV1Test {
+
+    /**
+     * 전략 패턴 사용 : 필들에 전략을 저장
+     */
+    @Test
+    void strategyV1() {
+        StrategyLogic1 strategyLogic1 = new StrategyLogic1();
+        ContextV1 contextV1 = new ContextV1(strategyLogic1);
+        contextV1.execute();
+
+        StrategyLogic2 strategyLogic2 = new StrategyLogic2();
+        ContextV1 contextV2 = new ContextV1(strategyLogic2);
+        contextV2.execute();
+    }
+
+    @Test
+    void strategyV2() {
+        ContextV1 contextV1 = new ContextV1(new Strategy() {  // 익명 클래스
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        });
+        contextV1.execute();
+
+        ContextV1 contextV2 = new ContextV1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        });
+        contextV2.execute();
+    }
+
+    @Test
+    void strategyV3() {
+        ContextV1 contextV1 = new ContextV1(() -> log.info("비즈니스 로직1 실행"));  // 람다
+        contextV1.execute();
+
+        ContextV1 contextV2 = new ContextV1(() -> log.info("비즈니스 로직2 실행"));
+        contextV2.execute();
+    }
+}
---
+++ src/test/java/hello/advanced/trace/strategy/ContextV2Test.java
@@ -0,0 +1,28 @@
+package hello.advanced.trace.strategy;
+
+import hello.advanced.trace.strategy.code.strategy.ContextV2;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ContextV2Test {
+
+    /**
+     * 전략 패턴 사용 : 파라미터로 전략을 전달
+     */
+    @Test
+    void strategyV1() {
+        ContextV2 contextV2 = new ContextV2();
+        contextV2.execute(new StrategyLogic1());
+        contextV2.execute(new StrategyLogic2());
+    }
+
+    @Test
+    void strategyV2() {
+        ContextV2 contextV2 = new ContextV2();
+        contextV2.execute(() -> log.info("비즈니스 로직1 실행"));
+        contextV2.execute(() -> log.info("비즈니스 로직 2 실행"));
+    }
+}
