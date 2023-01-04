2b1b0491691e19219896db821e53b795845aeff0
semi-cloud <sksmsrkdtpal@naver.com>
2023-01-04 16:06:35 +0900

    :sparkles: 로그 추적기 V5: 템플릿 메서드 패턴 적용

---
+++ src/main/java/hello/advanced/app/v4/OrderControllerV4.java
@@ -0,0 +1,30 @@
+package hello.advanced.app.v4;
+
+import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.template.AbstractTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV4 {
+
+    private final OrderServiceV4 orderService;
+    private final LogTrace trace;
+
+    /**
+     * 템플릿 매서드 패턴 적용
+     */
+    @GetMapping("/v4/request")
+    public String request(String itemId) {
+        AbstractTemplate<String> template = new AbstractTemplate<>(trace) {  // 익명 내부 클래스
+            @Override
+            protected String call() {
+                orderService.orderItem(itemId);
+                return "ok";
+            }
+        };
+        return template.execute("OrderController.request()");
+    }
+}
