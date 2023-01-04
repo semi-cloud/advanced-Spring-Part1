937576bfa96aae2e5ea595e1f31a0d545821f0cd
semi-cloud <sksmsrkdtpal@naver.com>
2023-01-04 17:31:55 +0900

    :white_check_mark: 템플릿 콜백 패턴 테스트 코드 추가

---
+++ src/test/java/hello/advanced/trace/strategy/code/TemplateCallBackTest.java
@@ -0,0 +1,16 @@
+package hello.advanced.trace.strategy.code;
+
+import hello.advanced.trace.strategy.code.template.TimeLogTemplate;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class TemplateCallBackTest {
+
+    @Test
+    void templateV1() {
+        TimeLogTemplate template = new TimeLogTemplate();
+        template.execute(() -> log.info("비즈니스 로직1 실행"));
+        template.execute(() -> log.info("비즈니스 로직2 실행"));
+    }
+}
